d9410baa0bc6df5dccf0474999b1eb39c7dd0a0e
SankalpPimpalkar <sankalp.pimpalkar.dev@gmail.com>
2026-08-11 22:23:10 +0530

    add crud and schema for events

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter
+from .schema import EventSchema, EventListSchema
 
 router = APIRouter()
 
 @router.get("/")
-def read_events():
-    return {
-        "items": [1,2,3]
-    }
+def read_events() -> EventListSchema:
+    return EventListSchema(results=[])
+
+@router.post("/")
+def create_event(event: EventSchema) -> EventSchema:
+    return EventSchema(id=event.id)
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    return EventSchema(id=event_id)
+
+@router.delete("/{event_id}")
+def delete_event(event_id: int) -> EventSchema:
+    return EventSchema(id=event_id)
---
+++ src/api/events/schema.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import List
+
+class EventSchema(BaseModel):
+    id: int
+
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
